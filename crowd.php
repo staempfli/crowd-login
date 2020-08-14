2518b7237c45014b8e191c54c7b78938c2ab1d32
Florian Auderset <f_auderset@hotmail.com>
2020-08-14 08:38:59 +0200

    [TASK] Bump version to 1.0.2

--- crowd.php
+++ crowd.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Stämpfli Crowd Login
  * Plugin URI:        https://www.staempfli.com
  * Description:       This plugin provides a login provider for atlassian crowd.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Florian Auderset
  * Author URI:        https://www.staempfli.com
  * License:           GPL-2.0+
